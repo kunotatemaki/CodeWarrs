2c2ce35917ae9fce0887f4272a73605e748eefcc
Raúl Feliz <raulfeliz@gmail.com>
2018-04-26 00:46:28 +0200

    add animation to seach box

--- app/src/main/java/com/rukiasoft/codewars/ui/search/SearchActivity.kt
+++ app/src/main/java/com/rukiasoft/codewars/ui/search/SearchActivity.kt
@@ -1,5 +1,7 @@
 package com.rukiasoft.codewars.ui.search
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
@@ -8,15 +10,26 @@ import android.support.graphics.drawable.AnimatedVectorDrawableCompat
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewAnimationUtils
 import com.rukiasoft.codewars.R
 import com.rukiasoft.codewars.databinding.ActivitySearchScreenBinding
 import com.rukiasoft.codewars.databinding.GlideBindingComponent
+import com.rukiasoft.codewars.model.RevealCoordinates
+import com.rukiasoft.codewars.utils.DeviceUtils
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_search_screen.view.*
+import timber.log.Timber
+import javax.inject.Inject
 
 class SearchActivity : BaseActivity() {
+
+    @Inject
+    lateinit var deviceUtils: DeviceUtils
 
     private lateinit var viewModel: SearchViewModel
     private lateinit var mBinding: ActivitySearchScreenBinding
 
+    private var revealCoordinates = RevealCoordinates()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -60,6 +73,7 @@ class SearchActivity : BaseActivity() {
         val avd = AnimatedVectorDrawableCompat.create(this.applicationContext, R.drawable.avd_anim_search_to_send)
         mBinding.fab.setImageDrawable(avd)
         avd?.start()
+        showCardAnimated()
         viewModel.searchCardVisible.set(true)
     }
 
@@ -68,7 +82,41 @@ class SearchActivity : BaseActivity() {
         val avd = AnimatedVectorDrawableCompat.create(this.applicationContext, R.drawable.avd_anim_send_to_search)
         mBinding.fab.setImageDrawable(avd)
         avd?.start()
-        viewModel.searchCardVisible.set(false)
+        hideCardAnimated()
+    }
+
+    private fun showCardAnimated(){
+        val x = mBinding.fab.right - (mBinding.fab.width / 2)
+        val y = mBinding.fab.bottom - (mBinding.fab.height / 2)
+        revealCoordinates.cx = x - deviceUtils.dipToPixels(8f).toInt()
+        revealCoordinates.cy = (mBinding.content.nameInput.container.bottom - mBinding.content.nameInput.container.top)/2
+        revealCoordinates.initialRadius = ((mBinding.fab.right - mBinding.fab.left) / 2).toFloat()
+        revealCoordinates.endRadius = Math.max(deviceUtils.getScreenWidth(), deviceUtils.getScreenHeight()).toFloat()
+        val anim = ViewAnimationUtils.createCircularReveal(mBinding.content.nameInput.root, revealCoordinates.cx, revealCoordinates.cy,
+                revealCoordinates.initialRadius, revealCoordinates.endRadius)
+
+        // make the view visible and start the animation
+
+        viewModel.searchCardVisible.set(true)
+        anim.duration = 300
+        anim.start()
+
+    }
+
+    private fun hideCardAnimated(){
+        val anim = ViewAnimationUtils.createCircularReveal(mBinding.content.nameInput.root, revealCoordinates.cx, revealCoordinates.cy,
+                revealCoordinates.endRadius, revealCoordinates.initialRadius)
+
+        anim.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+
+                viewModel.searchCardVisible.set(false)
+                //mBinding.content.nameInput.container.setVisibility(View.INVISIBLE)
+            }
+        })
+        anim.duration = 150
+        anim.start()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
